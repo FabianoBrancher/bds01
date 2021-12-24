402e15ceb58e514a9669530d4a7acc17995cdf04
Fabiano Brancher <fabiano.brancher@hotmail.com>
2021-12-24 11:28:21 -0300

    Tests passing

---
+++ src/main/java/com/devsuperior/bds01/controllers/DepartmentController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.bds01.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.devsuperior.bds01.dto.DepartmentDTO;
+import com.devsuperior.bds01.services.DepartmentService;
+
+@Controller
+@RequestMapping(value = "/departments")
+public class DepartmentController {
+	
+	@Autowired
+	private DepartmentService service;
+	
+	@GetMapping
+	public ResponseEntity<List<DepartmentDTO>> findAllDepartments() {
+		List<DepartmentDTO> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	
+	}
+}
---
+++ src/main/java/com/devsuperior/bds01/services/EmployeeService.java
@@ -0,0 +1,47 @@
+package com.devsuperior.bds01.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds01.dto.EmployeeDTO;
+import com.devsuperior.bds01.entities.Department;
+import com.devsuperior.bds01.entities.Employee;
+import com.devsuperior.bds01.repositories.DepartmentRepository;
+import com.devsuperior.bds01.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	private EmployeeRepository repository;
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<EmployeeDTO> findAllPaged(Pageable pageable) {
+		Page<Employee> page = repository.findAll(pageable);
+		
+		return page.map(e -> new EmployeeDTO(e));
+	}
+	
+	@Transactional
+	public EmployeeDTO insert(EmployeeDTO dto) {
+		Employee entity = new Employee();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+
+		return new EmployeeDTO(entity);
+	}
+	
+	
+	private void copyDtoToEntity(EmployeeDTO dto, Employee entity) {
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+//		entity.setDepartment(departmentRepository.getOne(dto.getDepartmentId()));
+		entity.setDepartment(new Department(dto.getDepartmentId(), null));
+	}
+}
